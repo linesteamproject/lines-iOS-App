01d60f20b253ba5e5c7c95bc669f3d782f3d6779
munyong <gjansdyd@gmail.com>
2022-07-30 09:20:55 +0900

    init

---
+++ lines_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lines_iOS
+//
+//  Created by mun on 2022/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
